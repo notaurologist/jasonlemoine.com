88616afc8d9936ac47f3582ef9e4ca8e7fea6f87
Jason LeMoine <github@lemoine.io>
2016-06-10 07:42:33 -0700

    feat: Added offline support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {install} from 'offline-plugin/runtime';
+install();
 import {Router, Route, IndexRedirect, Redirect, browserHistory} from 'react-router';
 import App from './components/App';
 import About from './components/About/About';
